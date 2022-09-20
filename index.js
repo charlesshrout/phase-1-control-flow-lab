723c746f8bbadf8f2977d9ec0d5d101f3533a004
Charles <charlie.david@me.com>
2022-09-20 15:02:49 -0600

    commit tree 7896233e82861dbd1930326dbca39149376b19e3

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet(value){
+  let result
+  if (value <= 400){result = 'This one is on me!'}
+  else if (value >= 400 && value <= 2000 ) {result = 'That will be twenty bucks.'}
+  else if (value > 2000 && value < 2500) {result = 'I will gladly take your thirty bucks.'}
+  else if (value > 2500) {result = 'No can do.'}
+  return result
+
+}
+function ternaryCheckCity(city){
+return city === 'NYC' ? "Ok, sounds good." : "No go."
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(moola)
+{ let result
+  switch (moola){
+    case "generous": result = "Thank you so much.";
+  break;
+case "not as generous" : result = "Thank you."
+break;
+default : result = "Bye."}
+return result}
